5b45d035c0e38f118523a89b1b2d4b41fa327fd0
Zack Lain <zacharytlain@gmail.com>
2024-04-13 15:33:08 -0400

    Refactor MoviePeopleJdbc.java to improve code readability and add a form to the page

--- WEB-INF/classes/MoviePeopleJdbc.java
+++ WEB-INF/classes/MoviePeopleJdbc.java
@@ -3,7 +3,7 @@ import javax.servlet.http.*;
 import java.io.*;
 import java.sql.*;
 
-public class MoviePeopleJdbc  extends HttpServlet {
+public class moviePeopleJdbc  extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
     response.setContentType("text/html");
@@ -25,40 +25,42 @@ public class MoviePeopleJdbc  extends HttpServlet {
           stmt = con.createStatement();
 
       // HTML header
-      out.println("<html><head><title>Person Report</title></head><body>");
+      out.println("<html><head><title>Movie People Report</title></head><body>");
 
-      String currentSort = request.getParameter("sort");
-
-      // Make a sorting drop down menu
-	  out.println("<center><form action='personJdbc' method='post'>" +
-      "<div>" +
-      "<label for='search1'>Movie Name:</label>" +
-      "<input type='text' id='search1' name='search1'>" +
-      "</div>" +
-      "<div>" +
-      "<label for='search2'>Full Person Name:</label>" +
-      "<input type='text' id='search2' name='search2'>" +
-      "</div>" +
-      "<div>" +
-      "<label for='search3'>Role Name:</label>" +
-      "<input type='text' id='search3' name='search3'>" +
-      "</div>" +
-      "<div>" +
-      "<button type='submit'>Submit</button>" +
-      "</div>" +
-      "</form></center>");
+      // Add a form to the page
+      // The form will have three text inputs for the user to enter search criteria
+	  out.println("<center><form action='moviePeopleJdbc' method='post'>" +
+                "<div>" +
+                    "<label for='search1'>Movie Name:</label>" +
+                    "<input type='text' id='search1' name='search1'>" +
+                "</div>" +
+                "<div>" +
+                    "<label for='search2'>Full Person Name:</label>" +
+                    "<input type='text' id='search2' name='search2'>" +
+                "</div>" +
+                "<div>" +
+                    "<label for='search3'>Role Name:</label>" +
+                    "<input type='text' id='search3' name='search3'>" +
+                "</div>" +
+                "<div>" +
+                    "<button type='submit'>Submit</button>" +
+                "</div>" +
+                "</form></center>");
 
 
       // Make table header
       out.println("<center><table border='1'><tr BGCOLOR='#cc cccc'>" +
-                  "<td>ID</td><td>Movie Name</td><td>First Name</td><td>Last Name</td><td>Role</td><td>Compensation</td></tr>");
+                  "<td>ID</td><td>Movie Name</td><td>Full Name</td><td>Role</td><td>Compensation</td></tr>");
 
       String movie = request.getParameter("search1");
       String person = request.getParameter("search2");
       String role = request.getParameter("search3");
+      Boolean movieEmpty = true;
+      Boolean personEmpty = true;
+      Boolean roleEmpty = true;
 
       String query = 
-          "SELECT m.m_title, ~p.p_firstname || \' \' || p.p_lastname, mr.mr_roleName, mpr.compensation " +
+          "SELECT mpr.mrp_id, m.m_title, p.p_firstName || \' \' || p.p_lastName as full_name, mr.mr_roleName, mpr.compensation " +
           "FROM movie_people_role mpr " +
           "JOIN person p ON mpr.p_id = p.p_id " +
           "JOIN movie m ON mpr.m_id = m.m_id " +
@@ -71,11 +73,17 @@ public class MoviePeopleJdbc  extends HttpServlet {
       }
 
       if (!person.isEmpty()) {
-        query += " WHERE LOWER(~p.p_firstname || \' \' || p.p_lastname) = \'" + person.toLowerCase() + "\'";
+        if (movie.isEmpty()) query += " WHERE";
+        else query += " AND";
+
+        query += " LOWER(p.p_firstName || \' \' || p.p_lastName) = \'" + person.toLowerCase() + "\'";
       }
 
       if (!role.isEmpty()) {
-        query += " WHERE LOWER(mr.mr_title) = \'" + role.toLowerCase() + "\'";
+        if (movie.isEmpty() && person.isEmpty()) query += " WHERE";
+        else query += " AND";
+
+        query += " LOWER(mr.mr_roleName) = \'" + role.toLowerCase() + "\'";
       }
 
       rs = stmt.executeQuery(query);
@@ -83,9 +91,10 @@ public class MoviePeopleJdbc  extends HttpServlet {
       // Process the result set
       while (rs.next()) {
         out.println("<tr>" + 
+                    "<td>" + rs.getString("mrp_id") + "</td>" +
                     "<td>" + rs.getString("m_title") + "</td>" +
-                    "<td>" + rs.getString("p_firstName") + " " + rs.getString("p_lastname") + "</td>" +
-                    "<td>" + rs.getString("mr_rolename") + "</td>" + 
+                    "<td>" + rs.getString("full_name") + "</td>" +
+                    "<td>" + rs.getString("mr_roleName") + "</td>" + 
                     "<td>" + rs.getString("compensation") + "</td>" + 
                     "</tr>");
       }
